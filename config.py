614c874eb586d726f4dda35a7e39e0c7ca84ff6f
Jubayerislam22 <102282828+Jubayerislam22@users.noreply.github.com>
2023-02-10 15:11:59 +0600

    Update config.py

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ class Config:
     LOG_GROUP=os.environ.get("LOG_GROUP", "")
 
     #Stream 
-    STREAM_URL=os.environ.get("STARTUP_STREAM", "http://jiocinema-localiptv.tk/supercricK.m3u8")
+    STREAM_URL=os.environ.get("STARTUP_STREAM", "https://youtu.be/OhFsCaadk94")
    
     #Database
     DATABASE_URI=os.environ.get("DATABASE_URI", "mongodb+srv://CrypticCloak:CrypticCloak@cluster0.qwpztbf.mongodb.net/?retryWrites=true&w=majority")
